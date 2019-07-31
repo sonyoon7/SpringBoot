12c3571de7b01e320eade276e95f6a4a60cd6123
EuniceSon <sonyoon7@gmail.com>
2019-07-31 20:17:27 +0900

    Revert "SpringBoot With JPA"
    This reverts commit 32fd0e147d39ab2c2772a9d673bfda393f654020.

--- firstboot/src/main/java/com/yuni/firstboot/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yuni.firstboot.repositories;
-
-import com.yuni.firstboot.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
